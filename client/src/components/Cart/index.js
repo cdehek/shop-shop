3c2eba6d6d8a1aa3ea799f2c82a725da8a2a5594
Christian <christian.dehek@gmail.com>
2021-01-19 14:25:34 -0700

    working on adding stripe

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,9 +5,18 @@ import "./style.css";
 import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { loadStripe } from '@stripe/stripe-js'
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { useLazyQuery } from '@apollo/react-hooks';
+
+
+const { stripePromise } = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx')
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
+
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise("cart", "get");
@@ -18,6 +27,15 @@ const Cart = () => {
       getCart();
     }
   }, [state.cart.length, dispatch]);
+  
+  //use useEffect to monitor when `data` changes for stripe checkout
+  useEffect(() => {
+      if (data) {
+          stripePromise.then((res) => {
+              res.redirectToCheckout({ sessionId: data.checkout.session });
+          });
+      }
+  }, [data]);
 
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
@@ -43,6 +61,20 @@ const Cart = () => {
 
   console.log(state);
 
+  function submitCheckout() {
+      const productIds = [];
+
+      state.cart.forEach((item) => {
+          for (let i = 0; i < item.purchaseQuantity; i++) {
+              productIds.push(item._id);
+          }
+      });
+
+      getCheckout({
+          variables: { products: productIds }
+      });
+  }
+
   return (
     <div className="cart">
       <div className="close" onClick={toggleCart}>
@@ -57,7 +89,7 @@ const Cart = () => {
           <div className="flex-row space-between">
             <strong>Total: ${calculateTotal()}</strong>
             {Auth.loggedIn() ? (
-              <button>Checkout</button>
+              <button onClick={submitCheckout}>Checkout</button>
             ) : (
               <span>(log in to check out)</span>
             )}
