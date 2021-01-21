ba9ed623c6fc866c2ee8db3ded53c7eaf7157837
Christian <christian.dehek@gmail.com>
2021-01-20 21:07:01 -0700

    updated detail.js

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -19,7 +19,7 @@ function Detail() {
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products, state } = state;
+  const { products, cart } = state;
 
   useEffect(() => {
     if (products.length) {
